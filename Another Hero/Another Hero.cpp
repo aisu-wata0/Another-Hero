8bea5b4ae3cee6a9e9fbfe12009178340535441d
bfs15 <bruno.serbena@gmail.com>
2016-12-16 21:17:50 -0200

    Changed window name

--- Another Hero/Another Hero.cpp
+++ Another Hero/Another Hero.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(200, 200), "Another Hero");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
 
