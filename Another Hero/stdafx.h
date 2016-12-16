08526ee9c5d5c04e475930cbf3faf8087fd06742
Bruno Freitas Serbena <bruno.serbena@gmail.com>
2016-12-16 19:06:45 -0200

    Changed directories

--- Another Hero/stdafx.h
+++ Another Hero/stdafx.h
@@ -13,3 +13,11 @@
 
 
 // TODO: reference additional headers your program requires here
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+
+#include <map>
+#include <iostream>
+#include <cassert>
