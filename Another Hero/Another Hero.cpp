5347b1b6bd5033a05ceae0dba813e00c563ab02c
bfs15 <bruno.serbena@gmail.com>
2016-12-17 10:34:43 -0200

    Moved movement handling inside event handling

--- Another Hero/Another Hero.cpp
+++ Another Hero/Another Hero.cpp
@@ -19,13 +19,15 @@ int main()
 		sf::Style::Close);
 
 	// pc character
+	/*TODO make a Character class,
+	discrete values for position (mult by TileSize)*/
 	sf::CircleShape pc(TileSizef/2);
 	pc.setFillColor(sf::Color::White);
 	pc.setPosition(4 * TileSizef, 4 * TileSizef);
 
 	// game loop clock
 	sf::Clock clock;
-	sf::Time elapsedTime;
+	sf::Int32 elapsedTime;
 
 	// timers
 	sf::Clock movementTimer;
@@ -34,7 +36,7 @@ int main()
 	while (window.isOpen())
 	{
 		// get elapsedTime since last loop
-		elapsedTime = clock.restart();
+		elapsedTime = clock.restart().asMicroseconds();
 
 		// treat events
 		sf::Event event;
@@ -42,37 +44,42 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
-		}
 
-		// Movement
-		/*if (movementTimer > 0.0f) {
-			movementTimer -= elapsedTime.asMilliseconds();
-		}*/
-		
-		if (movementTimer.getElapsedTime().asMilliseconds() >= movementRate) { 
-		//if (movementTimer <= 0.0f){
 			
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)
-			&& pc.getPosition().x < (BoardSize - 1)*TileSizef) {
-				pc.move(TileSizef, 0.0f);
-				//movementTimer = movementRate;
-				movementTimer.restart();
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)
-			&& pc.getPosition().y < (BoardSize-1)*TileSizef) {
-				pc.move(0.0f, TileSizef);
-				movementTimer.restart();
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)
-			&& pc.getPosition().x > 0.0f) {
-				pc.move(-TileSizef, 0.0f);
-				movementTimer.restart();
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)
-			&& pc.getPosition().y > 0.0f) {
-				pc.move(0.0f, -TileSizef);
-				movementTimer.restart();
-			}
+			if (event.type == sf::Event::KeyPressed) {
+
+				// Movement
+				/*if (movementTimer > 0.0f) {
+				movementTimer -= elapsedTime.asMilliseconds();
+				}*/
+
+				if (movementTimer.getElapsedTime().asMilliseconds() >= movementRate) {
+					//if (movementTimer <= 0.0f){
+
+					if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)
+					&& pc.getPosition().x < (BoardSize - 1)*TileSizef) {
+						pc.move(TileSizef, 0.0f);
+						//movementTimer = movementRate;
+						movementTimer.restart();
+					}
+					else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)
+					&& pc.getPosition().y < (BoardSize - 1)*TileSizef) {
+						pc.move(0.0f, TileSizef);
+						movementTimer.restart();
+					}
+					else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)
+					&& pc.getPosition().x > 0.0f) {
+						pc.move(-TileSizef, 0.0f);
+						movementTimer.restart();
+					}
+					else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)
+					&& pc.getPosition().y > 0.0f) {
+						pc.move(0.0f, -TileSizef);
+						movementTimer.restart();
+					}
+
+				}
+			} // end if (event.type == sf::Event::KeyPressed)
 
 		}
 
