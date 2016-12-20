05f21fbaa75db349292624bdbf550dfabfdc6731
bfs15 <bruno.serbena@gmail.com>
2016-12-20 16:06:34 -0200

    Added stats

--- Another Hero/Sources/stats.cpp
+++ Another Hero/Sources/stats.cpp
@@ -2,11 +2,17 @@
 
 void Stats::levelUp() 
 {
-	hp_ = (long)round(hp_*1.48);
-	mp_ = (long)round(mp_*1.48);
-	str_ = (long)round(str_*1.48);
-	def_ = (long)round(def_*1.48);
-	int_ = (long)round(int_*1.48);
-	res_ = (long)round(res_*1.48);
-	agi_ = (long)round(agi_*1.48);
+	base_hp_ = (long)round(base_hp_*1.5);
+	base_mp_ = (long)round(base_mp_*1.5);
+	base_str_ = (long)round(base_str_*1.5);
+	base_def_ = (long)round(base_def_*1.5);
+	base_int_ = (long)round(base_int_*1.5);
+	base_res_ = (long)round(base_res_*1.5);
+	base_agi_ = (long)round(base_agi_*1.5);
+
+	// applyBuffs();
 }
+
+//void Stats::applyBuffs() {
+//
+//}
--- Another Hero/Sources/stats.h
+++ Another Hero/Sources/stats.h
@@ -2,6 +2,31 @@
 
 class Stats
 {
+	//std::list<Buff> buffs;
+	// void applyBuffs();
+
+public:
+
+	// level 1 stats
+	long start_hp_;
+	long start_mp_;
+	long start_str_;
+	long start_def_;
+	long start_int_;
+	long start_res_;
+	long start_agi_;
+
+	// current level stats
+	long base_hp_;
+	long base_mp_;
+	long base_str_;
+	long base_def_;
+	long base_int_;
+	long base_res_;
+	long base_agi_;
+
+
+	// current battle stats
 	long hp_;
 	long mp_;
 	long str_;
@@ -10,6 +35,5 @@ class Stats
 	long res_;
 	long agi_;
 
-public:
 	void levelUp();
 };
